c1cb9caf2c56dda1a91c2718276375ec909e9a48
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-05-15 10:11:27 -0300

    Update and rename Parcial.java to ParcialArbolBinario.java

--- ParcialArbolBinario.java
+++ ParcialArbolBinario.java
@@ -45,44 +45,4 @@ public class Parcial {
  
 }
 
-		public  ListaGenerica<Integer> caminoAHojaMaxAlta(ArbolGeneral<Integer> unArbol){
-			Resultado devuelvo = new Resultado();
-			ListaGenerica<Integer> caminoActual = new ListaEnlazadaGenerica<Integer>();
-			ListaGenerica<Integer> caminoLargo = new ListaEnlazadaGenerica<Integer>();
-			int aux = 0;
-			if(!unArbol.esVacio()) caminoToHojaMaxAlta(unArbol,aux,devuelvo,caminoActual,caminoLargo);
-			return devuelvo.caminoLargo;
-			
-		}
-	 private static void caminoToHojaMaxAlta(ArbolGeneral<Integer> unArbol,int aux,Resultado max,ListaGenerica<Integer> caminoActual,
-				ListaGenerica<Integer> caminoLargo) {
-
-		 caminoActual.agregarFinal(unArbol.getDato());
-		 
-			if(unArbol.esHoja()) {
-				
-				if(aux+unArbol.getDato()>max.total) {
-
-					caminoLargo.comenzar();
-					while(!caminoLargo.fin())
-						caminoLargo.eliminar(caminoLargo.proximo());
-					
-					caminoActual.comenzar();
-					while(!caminoActual.fin()) {
-						caminoLargo.agregarFinal(caminoActual.proximo());
-					}
-					max.total = aux + unArbol.getDato();
-					max.caminoLargo=caminoLargo;
-				}
-				
-			} else {
-					ListaGenerica<ArbolGeneral<Integer>> hijos = unArbol.getHijos();
-					hijos.comenzar();
-					while(!hijos.fin()) {
-						caminoToHojaMaxAlta(hijos.proximo(),aux+unArbol.getDato(),max,caminoActual,caminoLargo);
-					}
-				}
-			caminoActual.eliminarEn(caminoActual.tamanio());
-			}
-	 
-	}
+	
